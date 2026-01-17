8d615083d7d732aa0d853e989ad9c21f73327cb1
erickok111-sketch <erickok111@gmail.com>
2026-01-17 20:30:40 +1000

    Delete airlie smart explorer directory

--- airlie smart explorer/Public/js/planner.js
+++
@@ -1,8 +0,0 @@
-export function buildPlanner(features) {
-  return {
-    morning: features.filter(f => f.properties.Morning_ok),
-    midday: features.filter(f => f.properties.Midday_ok),
-    afternoon: features.filter(f => f.properties.Afternoon_ok),
-    sunset: features.filter(f => f.properties.Sunset_ok)
-  };
-}
